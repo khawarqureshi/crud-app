0fcd83190d37d18bc848975f3614510465ff7a68
khawarqureshi <khawarqureshi08@gmail.com>
2022-01-12 16:57:16 +0500

    Added functionality to users component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,17 +1,91 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+import {
+  Table,
+  TableHead,
+  TableCell,
+  Paper,
+  TableRow,
+  TableBody,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
 import { getUsers } from "../services/api";
 
+const useStyles = makeStyles({
+  table: {
+    width: "90%",
+    margin: "50px 0 0 50px",
+  },
+  thead: {
+    "& > *": {
+      fontSize: 20,
+      background: "#000000",
+      color: "#FFFFFF",
+    },
+  },
+  row: {
+    "& > *": {
+      fontSize: 18,
+    },
+  },
+});
+
 const Users = () => {
+  const [users, setUsers] = useState([]);
+  const classes = useStyles();
+
   useEffect(() => {
     getAllUsers();
   }, []);
 
   const getAllUsers = async () => {
     const res = await getUsers();
-    console.log(res);
+    console.log(res.data);
+    setUsers(res.data);
   };
 
-  return <div>User Component</div>;
+  return (
+    <Table className={classes.table}>
+      <TableHead>
+        <TableRow className={classes.thead}>
+          <TableCell>Id</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Username</TableCell>
+          <TableCell>Title</TableCell>
+          <TableCell>Email</TableCell>
+          <TableCell>Phone</TableCell>
+          <TableCell>Actions</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow className={classes.row} key={user.id}>
+            <TableCell>{user.id}</TableCell>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>{user.username}</TableCell>
+            <TableCell>{user.title}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.phone}</TableCell>
+            <TableCell>
+              <Button
+                color="primary"
+                variant="contained"
+                style={{ marginRight: 10 }}
+                // component={Link}
+                to={`/edit/${user._id}`}
+              >
+                Edit
+              </Button>
+              <Button color="secondary" variant="contained">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default Users;
